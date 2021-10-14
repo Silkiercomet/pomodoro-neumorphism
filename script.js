9d252a78a92c623f990910321243d87308199007
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-10-14 15:29:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const watch = document.querySelector(".watch")
+let pomo;
+let sec = 59, min = 25
+
+const start = () =>{
+    pomo = setInterval(pomodoro,1000);
+ }
+
+const pomodoro = () => {
+        if(sec<59){
+        sec++;
+        document.querySelector(".sec").innerHTML = sec;
+        }else if(sec==59){
+        min--;
+        sec=1;
+        document.querySelector(".min").innerHTML = min;
+        document.querySelector(".sec").innerHTML = sec;         
+        }
+    
+        if(min==25){
+            clearInterval(pomo)
+        }
+}
+
+document.querySelector(".start").addEventListener("click", start)
